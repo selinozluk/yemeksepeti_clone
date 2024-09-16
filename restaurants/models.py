c730e7706f341415c57e6832c980361902494b78
Selin Özlük <selinnozlukk@gmail.com>
2024-09-16 14:06:21 +0300

    Add image field to MenuItem

--- restaurants/models.py
+++ restaurants/models.py
@@ -27,6 +27,12 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+    
+def upload_to(instance, filename):
+    # Dosyaları "Proje Yemekleri" klasörüne kaydetmek için özel bir yol
+    return f'Proje Yemekleri/{instance.restaurant.name}/{filename}'
+
+
 
 class MenuItem(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='menu_items')
@@ -34,12 +40,14 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image_url = models.URLField(null=True, blank=True)
+    image = models.ImageField(upload_to=upload_to, null=True, blank=True)  # Burada upload_to fonksiyonunu kullanıyoruz
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
-    
+
+
+
     # class Meta:
     #     db_table="menuitem"
