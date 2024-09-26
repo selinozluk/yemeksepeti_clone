15c664353c5ef2849ae256f4ae34946833e5b341
Selin Özlük <selinnozlukk@gmail.com>
2024-09-26 16:07:39 +0300

    Updated models with new upload_to function for image path

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,4 @@
+import os
 from django.db import models
 
 # Restoranlar için kategori modeli
@@ -5,7 +6,7 @@ class RestaurantCategory(models.Model):
     name = models.CharField(max_length=100)
 
     class Meta:
-     db_table = 'restaurants_restaurantcategory' 
+        db_table = 'restaurants_restaurantcategory'
 
     def __str__(self):
         return self.name
@@ -17,6 +18,14 @@ class MenuItemCategory(models.Model):
     def __str__(self):
         return self.name
 
+# Dosya yolu fonksiyonu
+def upload_to(instance, filename):
+    restaurant_name = instance.restaurant.name.replace(" ", "_")
+    filename_cleaned = filename.replace(" ", "_")
+    return f'Proje_Yemekleri/{restaurant_name}/{filename_cleaned}'
+
+
+# Restoran modeli
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
@@ -27,28 +36,17 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
-    
-def upload_to(instance, filename):
-    # Dosyaları "Proje Yemekleri" klasörüne kaydetmek için özel bir yol
-    return f'Proje Yemekleri/{instance.restaurant.name}/{filename}'
 
-
-
+# Menü öğesi modeli
 class MenuItem(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='menu_items')
     category = models.ForeignKey(MenuItemCategory, on_delete=models.CASCADE, related_name='menu_items', null=True, blank=True)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to=upload_to, null=True, blank=True)  # Burada upload_to fonksiyonunu kullanıyoruz
+    image = models.ImageField(upload_to=upload_to, null=True, blank=True)  # Özelleştirilmiş upload_to fonksiyonu
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
-    
 
     def __str__(self):
         return self.name
-
-
-
-    # class Meta:
-    #     db_table="menuitem"
