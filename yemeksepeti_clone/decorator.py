177648e46837b82e7f403eb21c785637ba863906
Selin Özlük <selinnozlukk@gmail.com>
2024-09-29 11:39:38 +0300

    Added RBAC decorator for role-based access control.

--- yemeksepeti_clone/decorator.py
+++ yemeksepeti_clone/decorator.py
@@ -1,30 +1,24 @@
 import jwt
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from functools import wraps
 
 User = get_user_model()
 
 def roles_required(*roles):
     def wrapper(func):
-        def wrapped(root,info,*args,**kwargs):
-            # if "authorization" not in info.context["request"].headers:
-            #     raise Exception("NOT AUTHORİZED")
-            # context =  info.context["request"].headers
-            # token = context.split(" ")[-1]
-            # secret, algorithm = settings.SECRET_KEY, settings.JWT_ALGORITHM
-            # payload = jwt.decode(token, secret, algorithm)
-            # if "email" not in payload:
-            #     raise Exception("NOT AUTHORİZED")
-            # try:
-            #     user =User.objects.get(email=payload["email"])
-            # except User.DoesNotExist:
-            #     raise Exception("USER NOT FOUND")
-            # info.context.user = user
+        @wraps(func)
+        def wrapped(root, info, *args, **kwargs):
             user = info.context.user
-            if "ADMIN" in roles and user.isAdmin:
-                func(*args,**kwargsr)
-            if "STAFF" in roles and user.is_staff:
-                func(*args,**kwargsr)
+            if not user.is_authenticated:
+                raise Exception("Giriş yapmadınız.")
             
+            if "ADMIN" in roles and not user.isAdmin:
+                raise Exception("Yetkiniz yok.")
+            
+            if "STAFF" in roles and not user.is_staff:
+                raise Exception("Yetkiniz yok.")
+            
+            return func(root, info, *args, **kwargs)
         return wrapped
     return wrapper
