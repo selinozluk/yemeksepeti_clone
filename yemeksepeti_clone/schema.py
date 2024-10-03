37c3c3e840b0333e8573067764a820421d397fe2
Selin Özlük <selinnozlukk@gmail.com>
2024-10-03 11:42:44 +0300

    Combine queries and mutations from all modules

--- yemeksepeti_clone/schema.py
+++ yemeksepeti_clone/schema.py
@@ -1,13 +1,16 @@
 import graphene
 from users.schema import Query as UsersQuery, Mutation as UsersMutation
 from restaurants.schema import Query as RestaurantsQuery, Mutation as RestaurantsMutation
-from orders.schema import OrderType, OrderItemType, CartType, CartItemType
+from orders.schema import Query as OrdersQuery, Mutation as OrdersMutation
 
-
-class Query(UsersQuery, RestaurantsQuery, graphene.ObjectType):
+# Tüm Query'leri birleştirme
+class Query(UsersQuery, RestaurantsQuery, OrdersQuery, graphene.ObjectType):
     pass
 
-class Mutation(UsersMutation, RestaurantsMutation, graphene.ObjectType):
+# Tüm Mutation'ları birleştirme
+class Mutation(UsersMutation, RestaurantsMutation, OrdersMutation, graphene.ObjectType):
     pass
 
+# Schema tanımı
 schema = graphene.Schema(query=Query, mutation=Mutation)
+
