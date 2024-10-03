510fefa05d57a3270de7cbf21ac2203892d9fe35
Selin Özlük <selinnozlukk@gmail.com>
2024-10-03 11:38:56 +0300

    Add models for restaurant and menu

--- restaurants/models.py
+++ restaurants/models.py
@@ -31,8 +31,8 @@ class Restaurant(models.Model):
     address = models.CharField(max_length=255)
     phone = models.CharField(max_length=15)
     category = models.ForeignKey(RestaurantCategory, on_delete=models.CASCADE, related_name='restaurants', null=True, blank=True)
-    createdAt = models.DateTimeField(auto_now_add=True)
-    updatedAt = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -45,8 +45,8 @@ class MenuItem(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to=upload_to, null=True, blank=True)  # Özelleştirilmiş upload_to fonksiyonu
-    createdAt = models.DateTimeField(auto_now_add=True)
-    updatedAt = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
