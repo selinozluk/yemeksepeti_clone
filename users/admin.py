a8bf86ff707dd1a824e4eb5a79f335f17be6f7a8
Selin Özlük <selinnozlukk@gmail.com>
2024-10-03 11:41:34 +0300

    Update user admin panel

--- users/admin.py
+++ users/admin.py
@@ -4,37 +4,38 @@ from .models import User
 
 class CustomUserAdmin(UserAdmin):
     model = User
-    list_display = ('email', 'firstName', 'lastName', 'isAdmin', 'is_staff', 'is_active')
-    list_filter = ('isAdmin', 'is_staff', 'is_active')
+    list_display = ('email', 'firstName', 'lastName', 'isAdmin', 'is_staff', 'is_active', 'is_customer')
+    list_filter = ('isAdmin', 'is_staff', 'is_active', 'is_customer')
     
     def has_view_permission(self, request, obj=None):
-        return request.user.is_staff or request.user.is_superuser
+        return request.user.is_staff or request.user.isAdmin
 
     def has_change_permission(self, request, obj=None):
-        return request.user.is_staff or request.user.is_superuser
+        return request.user.is_staff or request.user.isAdmin
 
     def has_delete_permission(self, request, obj=None):
-        return request.user.is_staff or request.user.is_superuser
+        return request.user.is_staff or request.user.isAdmin
 
     def has_add_permission(self, request):
-        return request.user.is_staff or request.user.is_superuser
+        return request.user.is_staff or request.user.isAdmin
     
-    # createdAt ve updatedAt alanlarını yalnızca okunabilir yapıyoruz
-    readonly_fields = ('createdAt', 'updatedAt')
+    # created_at ve updated_at alanlarını yalnızca okunabilir yapıyoruz
+    readonly_fields = ('created_at', 'updated_at')
 
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal Info', {'fields': ('firstName', 'lastName', 'birthDate')}),
-        ('Permissions', {'fields': ('isAdmin', 'is_staff', 'is_active')}),
-        ('Important dates', {'fields': ('last_login', 'createdAt', 'updatedAt')}),
+        (None, {'fields': ('email', 'password')}),  # Temel alanlar
+        ('Personal Info', {'fields': ('firstName', 'lastName', 'birthDate')}),  # Kişisel bilgiler
+        ('Permissions', {'fields': ('isAdmin', 'is_staff', 'is_active', 'is_customer')}),  # Yetkiler
+        ('Important dates', {'fields': ('last_login', 'created_at', 'updated_at')}),  # Tarih alanları
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'firstName', 'lastName', 'birthDate', 'isAdmin', 'is_staff', 'is_active')}
+            'fields': ('email', 'password1', 'password2', 'firstName', 'lastName', 'birthDate', 'isAdmin', 'is_staff', 'is_active', 'is_customer')},
         ),
     )
     search_fields = ('email',)
     ordering = ('email',)
 
 admin.site.register(User, CustomUserAdmin)
+
