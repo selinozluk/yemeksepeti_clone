7a1274277b099789c862fcfd4f40eb8dc292da85
Selin Özlük <selinnozlukk@gmail.com>
2024-10-14 12:41:51 +0300

    Add phone number to User model and initiate password reset

--- users/admin.py
+++ users/admin.py
@@ -4,7 +4,7 @@ from .models import User
 
 class CustomUserAdmin(UserAdmin):
     model = User
-    list_display = ('email', 'firstName', 'lastName', 'isAdmin', 'is_staff', 'is_active', 'is_customer')
+    list_display = ('email', 'firstName', 'lastName','phone_number', 'isAdmin', 'is_staff', 'is_active', 'is_customer')
     list_filter = ('isAdmin', 'is_staff', 'is_active', 'is_customer')
     
     def has_view_permission(self, request, obj=None):
@@ -24,17 +24,20 @@ class CustomUserAdmin(UserAdmin):
 
     fieldsets = (
         (None, {'fields': ('email', 'password')}),  # Temel alanlar
-        ('Personal Info', {'fields': ('firstName', 'lastName', 'birthDate')}),  # Kişisel bilgiler
+        ('Personal Info', {'fields': ('firstName', 'lastName', 'birthDate', 'phone_number')}),  # Kişisel bilgiler
         ('Permissions', {'fields': ('isAdmin', 'is_staff', 'is_active', 'is_customer')}),  # Yetkiler
         ('Important dates', {'fields': ('last_login', 'created_at', 'updated_at')}),  # Tarih alanları
     )
+
+    # Telefon numarası eklendi
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'firstName', 'lastName', 'birthDate', 'isAdmin', 'is_staff', 'is_active', 'is_customer')},
+            'fields': ('email', 'password1', 'password2', 'firstName', 'lastName', 'birthDate', 'phone_number', 'isAdmin', 'is_staff', 'is_active', 'is_customer')},
         ),
     )
-    search_fields = ('email',)
+    
+    search_fields = ('email','phone_number')
     ordering = ('email',)
 
 admin.site.register(User, CustomUserAdmin)
