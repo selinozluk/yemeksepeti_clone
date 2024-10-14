ac6f89674524312601b03c6835130a5a25925c88
Selin Özlük <selinnozlukk@gmail.com>
2024-10-14 12:43:37 +0300

    Add password reset URL to users

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 # users/urls.py
 
 from django.urls import path
-from .views import login, register
+from .views import login, register, password_reset
 
 
 urlpatterns = [
     path('login/', login, name='login'),
     path('register/', register, name='register'),
+    path('password-reset/', password_reset, name='password_reset'),  # Şifre sıfırlama URL'i
 ]
