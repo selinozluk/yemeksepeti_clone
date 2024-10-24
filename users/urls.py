e624e99befb770cd9a5f4ca6c05ea1ed970469e1
Selin Özlük <selinnozlukk@gmail.com>
2024-10-24 11:07:59 +0300

    Added URL patterns for login, register and password reset

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,8 @@
-# users/urls.py
-
 from django.urls import path
-from .views import login, register, password_reset
-
+from .views import user_login, register, password_reset  # login yerine user_login import edildi
 
 urlpatterns = [
-    path('login/', login, name='login'),
+    path('login/', user_login, name='login'),  # user_login olarak güncellendi
     path('register/', register, name='register'),
     path('password-reset/', password_reset, name='password_reset'),  # Şifre sıfırlama URL'i
 ]
